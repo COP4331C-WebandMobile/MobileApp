f20432e304fec3e8ec5b827bb9ccb9e23ca6e922
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-03-16 16:22:42 -0400

    configured firestore

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -4,8 +4,11 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:meta/meta.dart';
 
 import 'models/models.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpFailure implements Exception {}
+
+class AddUserFailure implements Exception {}
 
 class LogInWithEmailAndPasswordFailure implements Exception {}
 
@@ -17,36 +20,67 @@ class AuthenticationRepository {
   
   final auth.FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
-
+  final FirebaseFirestore _fireStore;
+  
   AuthenticationRepository({
     auth.FirebaseAuth firebaseAuth,
     GoogleSignIn googleSignIn,
+    FirebaseFirestore fireStore,
   }) : _firebaseAuth = firebaseAuth ?? auth.FirebaseAuth.instance,
-       _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
-  
+       _googleSignIn = googleSignIn ?? GoogleSignIn.standard(),
+       _fireStore = fireStore ?? FirebaseFirestore.instance;
 
+    //waits till not NULL
+
+  Future <void> addUser(
+    String email,
+    String firstName,
+    String lastName,
+    String phoneNumber, 
+    ) async{
+    try {
+    CollectionReference users = _fireStore.collection('users');
+    users.doc(email).set({
+      'first_name': firstName,
+      'last_name': lastName,
+      'phone_number': phoneNumber,
+      'house_name': ""
+      });
+    }
+    on Exception{
+      AddUserFailure();
+    }
+  }
   // Stream of User which will emit the current user whenever authentication state changes.
   // If user isn't authenticated then the empty user is returned.
   Stream<User> get user {
     return _firebaseAuth.authStateChanges().map((firebaseUser) {
       return firebaseUser == null ? User.empty : firebaseUser.toUser;
-
     });
   }
-
   Future<void> register({
     @required String email, 
-    @required String password
+    @required String password,
+    String firstName,
+    String lastName,
+    String phoneNumber
     }) async {
       assert (email != null && password != null);
-
       try {
-        await _firebaseAuth.createUserWithEmailAndPassword(
+          await _firebaseAuth.createUserWithEmailAndPassword(
           email: email,
           password: password,
         );
+        addUser(
+          email,
+          firstName = "testing",
+          lastName ="test",
+          phoneNumber ="testing",
+          );
+  
       }
-      on Exception {
+      on Exception catch(e){
+        print(e);
         throw SignUpFailure();
       }
     }
@@ -81,7 +115,6 @@ class AuthenticationRepository {
       }
     }
 }
-
 extension on auth.User {
   User get toUser {
     return User(
