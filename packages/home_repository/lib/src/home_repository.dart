aed6457c045305c1c881737bf6431f540b9bf443
freitas200 <42723796+freitas200@users.noreply.github.com>
2021-03-19 17:50:22 -0400

    Update home_repository.dart
    added add home function and some other incomplete functions

--- packages/home_repository/lib/src/home_repository.dart
+++ packages/home_repository/lib/src/home_repository.dart
@@ -1,30 +1,47 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
+import 'dart:async';
 
 class AddChoreFailure implements Exception {}
 
 class RemoveChoreFailure implements Exception {}
 
+class AddHouseFailure implements Exception {}
+
+class JoinHouseFailure implements Exception {}
 
 class HomeRepository {
-
   final FirebaseFirestore _firestore;
 
   HomeRepository({
     FirebaseFirestore fireStore,
-  })
-  : _firestore = fireStore ?? FirebaseFirestore.instance;
+  }) : _firestore = fireStore ?? FirebaseFirestore.instance;
 
   Future<void> addChore({
     @required String creator,
     @required String description,
     @required String name,
+    @required String choreID,
     String toDo,
   }) async {
-    
     //TODO: Implement addChore endpoint...
   }
+  Future<void> addHouse({
+    @required String houseName,
+    @required String member,
+  }) async {
+    try {
+      CollectionReference houses = _firestore.collection('houses');
+      houses
+          .doc(houseName)
+          .collection('roomates')
+          .doc('usernames')
+          .set({'member': member});
+    } on Exception {
+      AddHouseFailure();
+    }
+  }
 
+  Future<void> joinHouse(
+      {@required String houseName, @required String member}) async {}
 }
