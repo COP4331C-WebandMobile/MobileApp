6aea57dadaf53ee90f62789b62047a33cce65af5
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-03-22 15:57:18 -0400

    Added equtable

--- packages/chore_repository/lib/src/entities/chore_entity.dart
+++ packages/chore_repository/lib/src/entities/chore_entity.dart
@@ -13,30 +13,12 @@ class ChoreEntity extends Equatable {
 
   const ChoreEntity(this.task, this.id, this.note, this.complete);
 
-  Map<String, Object> toJson() {
-    return {
-      "complete": complete,
-      "task": task,
-      "note": note,
-      "id": id,
-    };
-  }
-
   @override
   List<Object> get props => [complete, id, note, task];
 
   @override
   String toString() {
     return 'ChoreEntity { complete: $complete, task: $task, note: $note, id: $id }';
-  }
-
-  static ChoreEntity fromJson(Map<String, Object> json) {
-    return ChoreEntity(
-      json["task"] as String,
-      json["id"] as String,
-      json["note"] as String,
-      json["complete"] as bool,
-    );
   }
 
   static ChoreEntity fromSnapshot(DocumentSnapshot snap) {
