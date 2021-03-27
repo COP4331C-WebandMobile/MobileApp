435fb80e010917b4fcf243d8e7d1c7cc94bf9703
freitas200 <42723796+freitas200@users.noreply.github.com>
2021-03-26 22:27:00 -0400

    Location
    Added some stuff for getting location still WIP

--- packages/map_repository/lib/src/model/map.dart
+++ packages/map_repository/lib/src/model/map.dart
@@ -1,1 +1,24 @@
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+Location location = new Location();
+
+Future<LocationData> data;
+Future<PermissionStatus> permission;
+Future<bool> service;
+
+void currentLocation() {
+  permission = location.requestPermission();
+  service = location.serviceEnabled();
+
+  if (service == false) {
+    location.requestService();
+  }
+
+  if (permission == PermissionStatus.granted && service == true) {
+    data = location.getLocation();
+  }
+}
