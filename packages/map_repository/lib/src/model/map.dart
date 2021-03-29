62a07cafcf95cb39e2d51b93a4283c1df7cc6ee6
freitas200 <42723796+freitas200@users.noreply.github.com>
2021-03-29 14:16:28 -0400

    fixes
    fixed manifest for android and added some test stuff to map

--- packages/map_repository/lib/src/model/map.dart
+++ packages/map_repository/lib/src/model/map.dart
@@ -10,15 +10,54 @@ Future<LocationData> data;
 Future<PermissionStatus> permission;
 Future<bool> service;
 
+//GoogleMap map;
+//GoogleMapController mapController;
+
 void currentLocation() {
   permission = location.requestPermission();
   service = location.serviceEnabled();
 
   if (service == false) {
-    location.requestService();
+    service = location.requestService();
   }
 
   if (permission == PermissionStatus.granted && service == true) {
     data = location.getLocation();
+  }
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Roomie Location'),
+          backgroundColor: Colors.yellow[300],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }
