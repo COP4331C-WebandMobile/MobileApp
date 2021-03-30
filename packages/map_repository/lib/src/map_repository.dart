198a09200ce18d4d6c8786c712546a8e1505f88b
freitas200 <42723796+freitas200@users.noreply.github.com>
2021-03-30 16:18:30 -0400

    map
    update

--- packages/map_repository/lib/src/map_repository.dart
+++ packages/map_repository/lib/src/map_repository.dart
@@ -1,32 +1,29 @@
 import 'package:location/location.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class PermissionDeniedException implements Exception {}
 
-
 class MapRepository {
+  final Location _location;
 
-final Location _location;
+  MapRepository(this._location);
 
-MapRepository(this._location);
+  Future<LocationData> getCurrentLocation() async {
+    var permission = await _location.requestPermission();
+    var service = await _location.serviceEnabled();
 
-Future<LocationData> getCurrentLocation() async {
+    // If location is disabled, request it.
+    if (service == false) {
+      service = await _location.requestService();
+    }
 
-  var permission = await _location.requestPermission();
-  var service = await _location.serviceEnabled();
-
-  // If location is disabled, request it.
-  if (service == false) {
-    service = await _location.requestService();
+    if (permission == PermissionStatus.granted && service == true) {
+      return await _location.getLocation();
+    } else {
+      throw PermissionDeniedException();
+    }
   }
 
-  
-  if (permission == PermissionStatus.granted && service == true) {
-    return await _location.getLocation();
-  }
-  else
-  {
-    throw PermissionDeniedException();
-  }
-}
-
+  Future<void> storeLocation() async {}
 }
