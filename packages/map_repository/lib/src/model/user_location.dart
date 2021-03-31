33bf0bafc69de9244089ba65655f6a0f752da8fa
freitas200 <42723796+freitas200@users.noreply.github.com>
2021-03-31 13:15:48 -0400

    Update user_location.dart
    finished up user_location.dart

--- packages/map_repository/lib/src/model/user_location.dart
+++ packages/map_repository/lib/src/model/user_location.dart
@@ -1,18 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
-
+import 'package:map_repository/src/entities/user_location_entity.dart';
 
 class UserLocation extends Equatable {
-
   final String id;
   final GeoPoint location;
   final Timestamp recordedTime;
 
   UserLocation(this.id, this.location, this.recordedTime);
-  
-  
 
+  UserLocation copyWith(
+      {String id, GeoPoint location, Timestamp recordedTime}) {
+    return UserLocation(id ?? this.id, location ?? this.location,
+        recordedTime ?? this.recordedTime);
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [id, location, recordedTime];
+
+  UserLocationEntity toEntity() {
+    return UserLocationEntity(id, location, recordedTime);
+  }
+
+  static UserLocation fromEntity(UserLocationEntity entity) {
+    return UserLocation(entity.id, entity.location, entity.recordedTime);
+  }
 }
