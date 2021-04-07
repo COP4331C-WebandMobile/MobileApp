0b59d5b3565d2911de4c77be25de293c5c7a169b
freitas200 <42723796+freitas200@users.noreply.github.com>
2021-04-06 23:38:07 -0400

    Update statistics_cubit.dart
    removed some print statements that were causing a freeze

--- lib/business_logic/statistics/cubit/statistics_cubit.dart
+++ lib/business_logic/statistics/cubit/statistics_cubit.dart
@@ -6,46 +6,36 @@ import 'package:meta/meta.dart';
 part 'statistics_state.dart';
 
 class StatsCubit extends Cubit<StatsState> {
- 
-
   final FirebaseFirestore _fireStore;
   final _home;
   final email;
 
-    StatsCubit(this._home,this.email) : this._fireStore = FirebaseFirestore.instance, super(StatsState.loading()){
-      getStats();
-    }
-  
+  StatsCubit(this._home, this.email)
+      : this._fireStore = FirebaseFirestore.instance,
+        super(StatsState.loading()) {
+    getStats();
+  }
 
   Future<void> getStats() async {
+    emit(StatsState.loading());
 
-       
+    List<Stat> stats = [];
 
-       emit(StatsState.loading());
-       
-       List<Stat> stats = [];
-
-       await _fireStore
+    await _fireStore
         .collection("roomates")
         .doc(_home)
         .collection('roomates')
         .doc(email)
-        .collection("chores").get().then((value) => value.docs.forEach((element) {
-          var description = element.data()["description"];
-          var completed= element.data()["completed"];
-          print(description);
+        .collection("chores")
+        .get()
+        .then((value) => value.docs.forEach((element) {
+              var description = element.data()["description"];
+              var completed = element.data()["completed"];
+              print(description);
 
-          stats.add(Stat(description,completed));
-          
-        }));
+              stats.add(Stat(description, completed));
+            }));
 
-        print("Testimgfdgf");
-
-        print(stats[0].completed);
-
-
-        emit(StatsState.loaded(stats));
-
-
-  }   
+    emit(StatsState.loaded(stats));
+  }
 }
